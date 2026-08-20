f877d4cc743f073de5b895ec72c0d9dee38ccf8a
xavierthecat943-glitch <xavierthecat943@gmail.com>
2026-08-20 16:39:46 -0400

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,13 @@
+# Tender Shell responses
+
+RESPONSES = {
+    "hello": "Hey! 👋",
+    "hi": "Hey there! 👋",
+    "hey": "What's up? 😎",
+    "thanks": "You're welcome!",
+    "thank you": "Anytime!",
+    "bye": "See ya! 👋",
+    "help": "I'm still learning, but I'm here!",
+}
+
+DEFAULT_RESPONSE = "I don't know what to say to that yet. 😭"
