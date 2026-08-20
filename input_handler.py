ebd85c87437901f7881a6f7da8b34da213a73a58
xavierthecat943-glitch <xavierthecat943@gmail.com>
2026-08-20 17:19:25 -0400

    Update input_handler.py

--- input_handler.py
+++ input_handler.py
@@ -2,7 +2,6 @@
 
 import readline
 
-from history import CommandHistory
 from suggestions import SuggestionEngine
 
 
@@ -11,7 +10,19 @@ class TenderInput:
         self.history = history
         self.suggestions = SuggestionEngine(history)
 
-    def get_input(self, prompt):
-        """Get input from the user."""
+        self._setup_readline()
 
-        return input(prompt)
+    def _setup_readline(self):
+        """Configure readline for command history."""
+
+        readline.parse_and_bind("set enable-keypad on")
+
+    def get_input(self, prompt):
+        """Get input from the user and save it to Tender history."""
+
+        command = input(prompt)
+
+        if command.strip():
+            self.history.add(command)
+
+        return command
